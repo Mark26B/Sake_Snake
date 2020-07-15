527546e41b15b72843ba0f729b490f765f938bd7
Marks Rozītis <rozitismarks@gmail.com>
2020-07-15 17:07:08 +0300

    version 0.5 Moving Sake Snake

--- Game.cs
+++ Game.cs
@@ -12,10 +12,6 @@ namespace Sake_Snake
 {
     public partial class Game : Form
     {
-        int horVelocity = 0;
-        int verVelicity = 0;
-        int step = 20;
-
         Area area = new Area();
         Snake snake = new Snake();
         Timer mainTimer = new Timer();
@@ -24,29 +20,69 @@ namespace Sake_Snake
         {
             InitializeComponent();
             InitializeGame();
+            InitializeTimer();
         }
 
-        private void InitiliazeTimer()
+        private void InitializeTimer()
         {
-            mainTimer.Interval = 500;
+            mainTimer.Interval = 200;
             mainTimer.Tick += new EventHandler(MainTimer_Tick);
             mainTimer.Start();
         }
         
         private void MainTimer_Tick(object sender, EventArgs e)
         {
-                
+            snake.Move();
         }
 
         private void InitializeGame()
         {
             this.Height = 600;
             this.Width = 600;
+
             this.Controls.Add(area);
             area.Top = 100;
             area.Left = 100;
             //area.Location = new Point(100, 100);
+
             snake.Render(this);
+
+            this.KeyDown += new KeyEventHandler(Game_KeyDown);
+        }
+
+        private void Game_KeyDown(object sneder, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Right:
+                    snake.VerticalVelocity = 0;
+                    if (snake.HorizontalVelocity != -1)
+                    {
+                        snake.HorizontalVelocity = 1;
+                    }
+                    break;
+                case Keys.Left:
+                    snake.VerticalVelocity = 0;
+                    if (snake.HorizontalVelocity != 1)
+                    {
+                        snake.HorizontalVelocity = -1;
+                    }
+                    break;
+                case Keys.Up:
+                    snake.HorizontalVelocity = 0;
+                    if (snake.VerticalVelocity != 1)
+                    {
+                        snake.VerticalVelocity = -1;
+                    }
+                    break;
+                case Keys.Down:
+                    snake.HorizontalVelocity = 0;
+                    if (snake.VerticalVelocity != -1)
+                    {
+                        snake.VerticalVelocity = 1;
+                    }
+                    break;
+            }
         }
     }
 }
--- Snake.cs
+++ Snake.cs
@@ -10,7 +10,11 @@ namespace Sake_Snake
 {
     class Snake
     {
-        List<PictureBox> snakePixels = new List<PictureBox>();
+        public int HorizontalVelocity { get; set; } = 0;
+        public int VerticalVelocity { get; set; } = 0;
+        public int Step { get; set; } = 20;
+
+        public List<PictureBox> snakePixels = new List<PictureBox>();
 
         public Snake()
         {
@@ -20,8 +24,8 @@ namespace Sake_Snake
         private void InitializeSnake()
         {
             AddSnakePixel(300, 300);
-            AddSnakePixel(300, 321);
-            AddSnakePixel(300, 342);
+            AddSnakePixel(300, 320);
+            AddSnakePixel(300, 340);
         }
 
         private void AddSnakePixel(int left, int top)
@@ -43,6 +47,22 @@ namespace Sake_Snake
                 form.Controls.Add(sp);
                 sp.BringToFront();
             }
+        }
+
+        public void Move()
+        {
+            if (this.HorizontalVelocity == 0 && this.VerticalVelocity == 0)
+            {
+                return;
+            }
+
+            for(int i = snakePixels.Count -1; i > 0; i--)
+            {
+                snakePixels[i].Location = snakePixels[i - 1].Location;
+            }
+
+            snakePixels[0].Left += this.HorizontalVelocity * this.Step;
+            snakePixels[0].Top += this.VerticalVelocity * this.Step;
         }
     }
 }
